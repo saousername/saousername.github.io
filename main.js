137bf7a2f706b47d6c5917ed4a691eca87d63658
saousername <59376248+saousername@users.noreply.github.com>
2020-07-26 11:51:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -16,23 +16,4 @@ function viewpip(e) {
 }
 function issuepip(e) {
     alert('Issue Reported!')
-
-const download = document.getElementById('download-pip')
-const view = document.getElementById('view-pip')
-const issue = document.getElementById('issue-pip')
-
-download.addEventListener('click',downloadpip)
-view.addEventListener('click',viewpip)
-issue.addEventListener('click',issuepip)
-
-function downloadpip(e) {
-    alert('Redirecting to download...')
-    window.location.href='/downloads/python/get-pip.py'
-}
-function viewpip(e) {
-    alert('Redirecting to online viewer...')
-    window.location.href='/downloads/python/get-pip.txt'
-}
-function issuepip(e) {
-    alert('Issue Reported!')
 }
