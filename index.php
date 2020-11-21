fda6be2110153abc3b4a185cfd713d76bc57c4ac
saousername <59376248+saousername@users.noreply.github.com>
2020-11-21 22:46:11 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+  <link rel="stylesheet" href="styling.css">
+</head>
+<body class='dbody'>
+
+<h1 class='header'>SAOPortal | Login</h1>
+<form action="welcome.php" method="POST">
+<input class='inputblock' placeholder='Username' type="Text" name="name"><br>
+<input class='inputblock' placeholder='Email' type="Email" name="email"><br>
+<input class='inputblock' placeholder='Password' type="Password" name="password"><br>
+<div>
+<input type="submit" class='block' value='Login'>
+</div>
+</form>
+
+</body>
+</html>
